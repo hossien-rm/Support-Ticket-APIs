bd2ad1d8d7577010445f032b231d6d56794fde24
Shayan Rafieemanesh <2hossienrafieemanesh@gmail.com>
2026-07-11 23:29:34 +0330

    feature: setup base of project

---
+++ src/main/java/com/task/springboot/SupportTicketAPI/SupportTicketAPIApplication.java
@@ -0,0 +1,13 @@
+package com.task.springboot.SupportTicketAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportTicketAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportTicketAPIApplication.class, args);
+	}
+
+}
