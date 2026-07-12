466fc51e9432be32bd63347bae4ac71a03844ef3
Shayan Rafieemanesh <2hossienrafieemanesh@gmail.com>
2026-07-12 15:13:20 +0330

    feature: add Entity class

---
+++ src/main/java/com/task/springboot/SupportTicketAPI/constant/TicketStatus.java
@@ -0,0 +1,7 @@
+package com.task.springboot.SupportTicketAPI.constant;
+
+public enum TicketStatus {
+    OPEN,
+    IN_PROGRESS,
+    CLOSE
+}
---
+++ src/main/java/com/task/springboot/SupportTicketAPI/entity/TicketEntity.java
@@ -0,0 +1,33 @@
+package com.task.springboot.SupportTicketAPI.entity;
+
+import com.task.springboot.SupportTicketAPI.constant.TicketStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="ticket_entity")
+public class TicketEntity {
+
+    @Id
+    @Column(name = "ticket_id")
+    private Long ticketId;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "ticket_status")
+    private TicketStatus ticketStatus;
+
+
+}
