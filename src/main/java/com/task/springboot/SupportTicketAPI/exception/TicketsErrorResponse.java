2e1b31b6752847276c37d035392c884a055714a8
Shayan Rafieemanesh <2hossienrafieemanesh@gmail.com>
2026-07-13 18:51:14 +0330

    Fixed: use lombok in TicketsErrorResponse

--- src/main/java/com/task/springboot/SupportTicketAPI/exception/TicketsErrorResponse.java
+++ src/main/java/com/task/springboot/SupportTicketAPI/exception/TicketsErrorResponse.java
@@ -1,40 +1,14 @@
 package com.task.springboot.SupportTicketAPI.exception;
 
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class TicketsErrorResponse {
     private int status ;
     private String message;
     private Long timeStamp;
 
-    public TicketsErrorResponse(int status, String message, Long timeStamp) {
-        this.status = status;
-        this.message = message;
-        this.timeStamp = timeStamp;
-    }
-    public TicketsErrorResponse(){
-
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public Long getTimeStamp() {
-        return timeStamp;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public void setTimeStamp(Long timeStamp) {
-        this.timeStamp = timeStamp;
-    }
 }
