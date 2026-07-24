848bb0820445c951ea0144b3bd1c1e2bb950bd1e
Shayan Rafieemanesh <2hossienrafieemanesh@gmail.com>
2026-07-24 13:05:07 +0330

    Feature: add some annotation for validation

--- src/main/java/com/task/springboot/SupportTicketAPI/dto/TicketDto.java
+++ src/main/java/com/task/springboot/SupportTicketAPI/dto/TicketDto.java
@@ -1,9 +1,19 @@
 package com.task.springboot.SupportTicketAPI.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
+
 public record TicketDto(
+
         Long ticketId,
+
+        @NotBlank(message = "title shouldn't null or empty!" )
         String title,
+
+        @NotBlank(message= "description should not be null or empty")
         String description,
+
+        @NotBlank(message = "ticket status shouldn't null or empty!")
         String ticketStatus
 ) {
 }
