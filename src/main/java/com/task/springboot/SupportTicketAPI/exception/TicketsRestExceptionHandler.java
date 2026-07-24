241e29f065d3aaa868ba6685396d205811cefff1
Shayan Rafieemanesh <2hossienrafieemanesh@gmail.com>
2026-07-24 13:06:27 +0330

    Feature: add Exception for handle MethodArgumentNotValidException

--- src/main/java/com/task/springboot/SupportTicketAPI/exception/TicketsRestExceptionHandler.java
+++ src/main/java/com/task/springboot/SupportTicketAPI/exception/TicketsRestExceptionHandler.java
@@ -1,34 +1,52 @@
 package com.task.springboot.SupportTicketAPI.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class TicketsRestExceptionHandler {
 
-    //handle just Not found Exception.
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler
-    public ResponseEntity<TicketsErrorResponse> handleException(TicketsNotFoundException exc){
+    public Map<String,String> handleValidArgument(MethodArgumentNotValidException validException){
+
+        Map<String,String> errorMap=new HashMap<>();
+        validException.getBindingResult().getFieldErrors()
+                .forEach(error-> errorMap.put(error.getField(),error.getDefaultMessage()));
+        return errorMap;
+    }
+
+
+
+    //handle just Not found Exception.
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler
+    public TicketsErrorResponse handleException(TicketsNotFoundException exc){
 
         TicketsErrorResponse error=new TicketsErrorResponse();
         error.setStatus(HttpStatus.NOT_FOUND.value());
         error.setMessage(exc.getMessage());
         error.setTimeStamp(System.currentTimeMillis());
 
-        return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+        return error;
     }
 
     // handle all Exception.
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler
-    public ResponseEntity<TicketsErrorResponse> handleAllException(Exception ex){
+    public TicketsErrorResponse handleAllException(Exception ex){
 
         TicketsErrorResponse error=new TicketsErrorResponse();
         error.setStatus(HttpStatus.BAD_REQUEST.value());
         error.setMessage(ex.getMessage());
         error.setTimeStamp(System.currentTimeMillis());
 
-        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
+        return error;
     }
 }
